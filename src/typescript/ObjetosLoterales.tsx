cc32ab260c1922e5b12c7eaea291ad9bf776e467
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2023-09-20 15:41:29 -0400

    Type script basics

---
+++ src/typescript/ObjetosLoterales.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface Persona {
+  nombreCompelto: string;
+  edad: string;
+  direccion: Direccion;
+}
+
+interface Direccion {
+  pais: string;
+  casaNo: number;
+}
+
+export const ObjetosLoterales = () => {
+  const persona: Persona = {
+    nombreCompelto: "Fernando",
+    edad: "35",
+    direccion: {
+      pais: "Canadá",
+      casaNo: 615,
+    },
+  };
+
+  return (
+    <>
+      <h3>Objetos Literalesa</h3>
+
+      <code>
+        <pre>{JSON.stringify(persona, null, 2)}</pre>
+      </code>
+    </>
+  );
+};
